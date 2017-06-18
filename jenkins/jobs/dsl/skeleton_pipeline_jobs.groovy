96b5e7a6449a37b568c5479478ec99f7e812df5c
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2017-06-18 14:08:18 -0500

    Test yaml command

--- jenkins/jobs/dsl/skeleton_pipeline_jobs.groovy
+++ jenkins/jobs/dsl/skeleton_pipeline_jobs.groovy
@@ -1,6 +1,15 @@
 
 import pluggable.scm.*;
 
+def datas = readYaml text: """
+something: 'my datas'
+size: 3
+isEmpty: false
+"""
+        assert datas.something == 'my datas'
+        assert datas.size == 3
+        assert datas.isEmpty == false
+
 SCMProvider scmProvider = SCMProviderHandler.getScmProvider("${SCM_PROVIDER_ID}", binding.variables)
 
 // Folders
