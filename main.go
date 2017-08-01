ee39591dc7fb286b8efeff653a52242a162aa169
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2017-08-01 15:45:22 -0500

    Read configuration

--- main.go
+++ main.go
@@ -1,28 +1,35 @@
 package main
 
 import (
+	"log"
 	"os"
 	"path/filepath"
 
 	"github.com/jeffail/tunny"
+	"github.com/thetonymaster/test_catridge/configuration"
 	"github.com/thetonymaster/test_catridge/provider/container"
 	"github.com/thetonymaster/test_catridge/provider/test"
 )
 
 func main() {
 
-	pool, _ := tunny.CreatePool(1, func(f interface{}) interface{} {
+	args := os.Args[1:]
+
+	conf, err := configuration.Read(args[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pool, _ := tunny.CreatePool(conf.Containers.Limit, func(f interface{}) interface{} {
 		input, _ := f.(func())
 		input()
 		return nil
 	}).Open()
 	defer pool.Close()
 
-	args := os.Args[1:]
-
-	dir, _ := filepath.Abs(filepath.Dir(args[0]))
-	containerProvider := container.NewDockerComposeGenerator(args)
-	jUnitTestProvider := test.NewJUnit(containerProvider, pool)
+	dir, _ := filepath.Abs(filepath.Dir(conf.Tests["junit"].Path))
+	containerProvider := container.NewDockerComposeGenerator([]string{conf.Tests["junit"].Path})
+	jUnitTestProvider := test.NewJUnit(containerProvider, conf.Tests["junit"].Target, pool)
 	tasks := jUnitTestProvider.GetFiles(dir + "/src/test/")
 	jUnitTestProvider.RunTask(tasks)
 
